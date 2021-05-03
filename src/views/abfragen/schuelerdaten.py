fdfe29a2a6a141cacefde53b05e3cebffe88d3b9
tza@spaz <christian.macht@gmail.com>
2021-05-03 19:07:50 +0200

    Prevent empty values in step 1

--- src/views/abfragen/schuelerdaten.py
+++ src/views/abfragen/schuelerdaten.py
@@ -5,28 +5,17 @@ from src.views.macros import radio, text, select
 
 def process():
     """Process the form data entered at step1()
-    Todo: Errors with None.strip() and msa
     """
     error = []
 
-    vorname = text(request, 'vorname', error).strip()
-    nachname = text(request, 'nachname', error).strip()
+    vorname = text(request, 'vorname', error)
+    nachname = text(request, 'nachname', error)
     jahrgang = current_app.config['JAHRGANG']
     klasse = select(request, 'klasse', error)
     msa = radio(request, 'msa', error)
     profil = select(request, 'profilbuchstabe', error)
     freierlk = select(request, 'freierlk', error)
     pflichtstundenzahl = radio(request, 'pflichtstundenzahl', error, 'g8', 'g9')
-
-    cache.write(hash='session', mapping={
-        'vorname': vorname,
-        'nachname': nachname,
-        'jahrgang': jahrgang,
-        'klasse': klasse,
-        'profilbuchstabe': profil,
-        'freierlk': freierlk,
-        'msa': str(msa),
-    })
 
     if (profil == 'A' and freierlk == 'deu') \
             or (profil == 'B' and freierlk == 'eng') \
@@ -39,6 +28,15 @@ def process():
         stunden = 124
 
     if not error:
+        cache.write(hash='session', mapping={
+            'vorname': vorname.strip(),
+            'nachname': nachname.strip(),
+            'jahrgang': jahrgang,
+            'klasse': klasse,
+            'profilbuchstabe': profil,
+            'freierlk': freierlk,
+            'msa': str(msa),
+        })
         cache.write('pflichtstundenzahl', stunden, hash='session')
 
     return error
